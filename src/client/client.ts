f9de419ddfd2a05a4f8601e2474b09154b288c48
Igor Zvyagin <spirit-drive@yandex.ru>
2023-09-12 12:18:56 +0300

    feat: prepare the apollo client for subscriptions

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,14 +1,38 @@
-import { ApolloClient, createHttpLink, InMemoryCache, from } from '@apollo/client';
+import { ApolloClient, createHttpLink, InMemoryCache, from, split } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import { errorLink } from 'src/client/errorLink';
 import { storage } from 'src/utils/storage';
-import { URL } from 'src/client/config';
+import { URL, WS_URL } from 'src/client/config';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { createClient } from 'graphql-ws';
+import { getMainDefinition } from '@apollo/client/utilities';
 import possibleTypes from './possibleTypes.json';
 import { TOKEN_KEY } from '../store/token';
+
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: `${WS_URL}/subscriptions`,
+    connectionParams: () => {
+      const token = storage.get(TOKEN_KEY);
+      return {
+        authorization: token ? `Bearer ${token}` : '',
+      };
+    },
+  })
+);
 
 const httpLink = createHttpLink({
   uri: `${URL}/graphql`,
 });
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return definition.kind === 'OperationDefinition' && definition.operation === 'subscription';
+  },
+  wsLink,
+  httpLink
+);
 
 const authLink = setContext((_, { headers }) => {
   const token = storage.get(TOKEN_KEY);
@@ -21,7 +45,7 @@ const authLink = setContext((_, { headers }) => {
 });
 
 export const client: ApolloClient<unknown> = new ApolloClient({
-  link: from([errorLink, authLink, httpLink]),
+  link: from([errorLink, authLink, splitLink]),
   cache: new InMemoryCache({
     possibleTypes,
   }),
--- src/client/config.ts
+++ src/client/config.ts
@@ -5,3 +5,4 @@ export const NOT_AUTHORIZED_CODE = 'ERR_JWT_ERROR';
 export const PORT = 4002;
 export const HOST = 'localhost';
 export const URL = process.env.LOCAL === 'true' ? `http://${HOST}:${PORT}` : PUBLIC_PATH;
+export const WS_URL = URL.replace(/^http/, 'ws');
