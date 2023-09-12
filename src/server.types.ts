fb18ec93ca9d9660ad483c5a4e5b7d7eaaa6d2dc
Igor Zvyagin <spirit-drive@yandex.ru>
2023-09-12 18:47:52 +0300

    chore: update schema

--- src/server.types.ts
+++ src/server.types.ts
@@ -15,9 +15,48 @@ export type Scalars = {
   Date: { input: any; output: any; }
 };
 
+export type Animal = Bird | Cat | Dog;
+
+export type AnimalInput = {
+  age?: InputMaybe<Scalars['Int']['input']>;
+  comment?: InputMaybe<Scalars['String']['input']>;
+  diseaseIds?: InputMaybe<Array<Scalars['ID']['input']>>;
+  doctorId?: InputMaybe<Scalars['ID']['input']>;
+  name: Scalars['String']['input'];
+  type: AnimalType;
+};
+
+export enum AnimalType {
+  Bird = 'Bird',
+  Cat = 'Cat',
+  Dog = 'Dog'
+}
+
 export type AuthResult = {
   __typename?: 'AuthResult';
   token: Scalars['String']['output'];
+};
+
+export type Bird = {
+  __typename?: 'Bird';
+  age?: Maybe<Scalars['Int']['output']>;
+  comment?: Maybe<Scalars['String']['output']>;
+  diseases: Array<Maybe<Disease>>;
+  doctor?: Maybe<User>;
+  id: Scalars['ID']['output'];
+  name: Scalars['String']['output'];
+  updatedAt?: Maybe<Scalars['Date']['output']>;
+};
+
+export type Cat = {
+  __typename?: 'Cat';
+  age?: Maybe<Scalars['Int']['output']>;
+  comment?: Maybe<Scalars['String']['output']>;
+  diseases: Array<Maybe<Disease>>;
+  doctor?: Maybe<User>;
+  id: Scalars['ID']['output'];
+  name: Scalars['String']['output'];
+  updatedAt?: Maybe<Scalars['Date']['output']>;
 };
 
 export type ChangePasswordInput = {
@@ -25,16 +64,99 @@ export type ChangePasswordInput = {
   password: Scalars['String']['input'];
 };
 
+export type Disease = {
+  __typename?: 'Disease';
+  desc: Scalars['String']['output'];
+  id: Scalars['ID']['output'];
+  name: Scalars['String']['output'];
+  type: DiseaseType;
+};
+
+export type DiseaseInput = {
+  desc: Scalars['String']['input'];
+  name: Scalars['String']['input'];
+  type: DiseaseType;
+};
+
+export enum DiseaseType {
+  Broken = 'broken',
+  Cold = 'cold',
+  Parasites = 'parasites',
+  Stomach = 'stomach'
+}
+
+export type Dog = {
+  __typename?: 'Dog';
+  age?: Maybe<Scalars['Int']['output']>;
+  comment?: Maybe<Scalars['String']['output']>;
+  diseases: Array<Maybe<Disease>>;
+  doctor?: Maybe<User>;
+  id: Scalars['ID']['output'];
+  name: Scalars['String']['output'];
+  updatedAt?: Maybe<Scalars['Date']['output']>;
+};
+
+export type Medicine = {
+  __typename?: 'Medicine';
+  heal: Array<DiseaseType>;
+  id: Scalars['ID']['output'];
+  name: Scalars['String']['output'];
+};
+
+export type MedicineInput = {
+  heal: Array<DiseaseType>;
+  name: Scalars['String']['input'];
+};
+
 export type Mutation = {
   __typename?: 'Mutation';
+  addAnimal: Animal;
+  addDisease: Disease;
+  addMedicine: Medicine;
   profile?: Maybe<ProfileMutations>;
+  updateAnimal: Animal;
+  updateDisease: Disease;
+  updateMedicine: Medicine;
+};
+
+
+export type MutationAddAnimalArgs = {
+  input: AnimalInput;
+};
+
+
+export type MutationAddDiseaseArgs = {
+  input: DiseaseInput;
+};
+
+
+export type MutationAddMedicineArgs = {
+  input: MedicineInput;
+};
+
+
+export type MutationUpdateAnimalArgs = {
+  id: Scalars['ID']['input'];
+  input: AnimalInput;
+};
+
+
+export type MutationUpdateDiseaseArgs = {
+  id: Scalars['ID']['input'];
+  input: DiseaseInput;
+};
+
+
+export type MutationUpdateMedicineArgs = {
+  id: Scalars['ID']['input'];
+  input: MedicineInput;
 };
 
 export type Profile = {
   __typename?: 'Profile';
   email: Scalars['String']['output'];
   id: Scalars['ID']['output'];
-  name: Scalars['String']['output'];
+  name?: Maybe<Scalars['String']['output']>;
   signUpDate: Scalars['Date']['output'];
 };
 
@@ -75,7 +197,11 @@ export type ProfilePasswordMutationsChangeArgs = {
 
 export type Query = {
   __typename?: 'Query';
+  animals: Array<Animal>;
+  diseases: Array<Disease>;
+  medicines: Array<Medicine>;
   profile?: Maybe<Profile>;
+  users: Array<User>;
 };
 
 export type ResetPassword = {
@@ -83,13 +209,21 @@ export type ResetPassword = {
   success: Scalars['Boolean']['output'];
 };
 
+export type Subscription = {
+  __typename?: 'Subscription';
+  updatedAnimal: Animal;
+  updatedDisease: Disease;
+  updatedMedicine: Medicine;
+  updatedUser: User;
+};
+
 export type UpdateProfileInput = {
-  name?: InputMaybe<Scalars['String']['input']>;
+  name: Scalars['String']['input'];
 };
 
 export type User = {
   __typename?: 'User';
   id: Scalars['ID']['output'];
-  name: Scalars['String']['output'];
+  name?: Maybe<Scalars['String']['output']>;
   signUpDate: Scalars['Date']['output'];
 };
