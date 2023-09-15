72c608afb79b2486c4a7e71116fadfffd4741fce
Igor Zvyagin <spirit-drive@yandex.ru>
2023-09-15 16:10:30 +0300

    feat(src/components/Selections/DiseaseSelect): made

---
+++ src/components/Selections/DiseaseSelect/DiseaseSelect.tsx
@@ -0,0 +1,50 @@
+import React, { FC, useState } from 'react';
+import { Select, SelectProps } from 'antd';
+import cn from 'clsx';
+import { Disease } from 'src/server.types';
+import { TextSelection } from 'src/components/TextSelection';
+import s from './DiseaseSelect.sass';
+
+export type DiseasesSelectProps = SelectProps & {
+  className?: string;
+  items: Disease[];
+  value: Disease['id'];
+  onBlur?: SelectProps['onBlur'];
+  disabled?: SelectProps['disabled'];
+  placeholder?: SelectProps['placeholder'];
+  onChange: (value: Disease['id']) => void;
+};
+
+export const DiseaseSelect: FC<DiseasesSelectProps> = ({
+  className,
+  disabled,
+  placeholder,
+  onBlur,
+  items,
+  value,
+  onChange,
+}) => {
+  const [search, onSearch] = useState<string>();
+
+  const filterOption = (input: string, option: { title: string; value: string }) =>
+    (option?.title ?? '').toLowerCase().includes(input.toLowerCase());
+
+  return (
+    <Select
+      onBlur={onBlur}
+      disabled={disabled}
+      className={cn(s.root, className)}
+      filterOption={filterOption}
+      value={value}
+      placeholder={placeholder}
+      onChange={onChange}
+      onSearch={onSearch}
+    >
+      {items?.map((item) => (
+        <Select.Option key={item.id} title={item.name}>
+          <TextSelection text={item.name} selection={search} />
+        </Select.Option>
+      ))}
+    </Select>
+  );
+};
