99bd40c942f4e07b74a4b26e2f92bc9ebfeac6d3
Alexander Samak <voprosy262@gmail.com>
2024-10-02 14:22:18 +0300

    feat: add native drag-n-drop example

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -4,11 +4,69 @@ import { Page } from 'src/components/Page';
 import { ManagePanel } from 'src/components/ManagePanel';
 import s from './Home.sass';
 
+// const NativeDragComponent = () => {
+//   const [list, setList] = useState([{ currentList: 'first' }]);
+
+//   return (
+//     <div
+//       onDragOver={(e) => e.preventDefault()}
+//       onDrop={(e) => {
+//         const idElement = e.dataTransfer.getData('text');
+
+//         if (idElement === 'first') {
+//           setList([{ currentList: 'second' }]);
+//         } else {
+//           setList([{ currentList: 'first' }]);
+//         }
+//       }}
+//     >
+//       <div style={{ width: '300px', height: '300px', background: 'blue' }}>
+//         {list.map(({ currentList }, index) => {
+//           if (currentList === 'first') {
+//             return (
+//               <div
+//                 key={index}
+//                 id={currentList}
+//                 style={{ width: '100px', height: '100px', background: 'red' }}
+//                 draggable
+//                 onDragStart={(e: any) => {
+//                   console.log('e.target.id', e.target.id);
+//                   e.dataTransfer.setData('text', e.target.id);
+//                 }}
+//               />
+//             );
+//           }
+//         })}
+//       </div>
+
+//       <div style={{ width: '300px', height: '300px', background: 'green' }}>
+//         {list.map(({ currentList }, index) => {
+//           if (currentList === 'second') {
+//             return (
+//               <div
+//                 key={index}
+//                 id={currentList}
+//                 style={{ width: '100px', height: '100px', background: 'red' }}
+//                 draggable
+//                 onDragStart={(e: any) => {
+//                   console.log('e.target.id', e.target.id);
+//                   e.dataTransfer.setData('text', e.target.id);
+//                 }}
+//               />
+//             );
+//           }
+//         })}
+//       </div>
+//     </div>
+//   );
+// };
+
 export const Home: FC = () => {
   const { t } = useTranslation();
 
   return (
     <Page title={t`screens.HomeScreen.title`} className={s.root}>
+      {/* <NativeDragComponent /> */}
       <ManagePanel />
     </Page>
   );
